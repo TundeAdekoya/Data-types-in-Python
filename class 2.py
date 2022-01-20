51709c6cfe97bcf2c3a5fc794b506612f2ba3e05
Tunde <adekoyatunde101@gmail.com>
2022-01-20 15:52:28 +0100

    first commit

---
+++ class 2.py
@@ -0,0 +1,137 @@
+#string concatination
+#a = 'hello'
+#b = 'world'
+#c=a+b
+#print(c)
+
+#my_string='I am a boy'
+#print(my_string[3])
+#print(my_string[2:])
+#print(my_string[-9:-5:2])
+
+#my_string='This is 1000 naira'
+#st2 = 'This 500 is lovely'
+#a1=(my_string[8])
+#a2=(st2[5:9])
+#a3=a1+a2
+#print(a3)
+
+#first_num = my_string[8:12]
+#second_num = st2[5:8]
+#added = int(first_num) + int(second_num)
+#print(added)
+#print(f'The sum of {first_num} and {second_num} is {added}')
+#print(f'The sum of %s and %s is %d' % (first_num, second_num, added))
+
+#print("This is ayo's book")
+#print('This is ayo\'s book')
+
+#print("This is a book\n This is another line")
+#print("Dear, Buhari, \nPlease don't run for president again. \n\tYours in the Country.\n\t\tAstroverse Team.")
+
+
+
+
+
+# print("""This is a wonderful string                                                 
+
+
+#                     Yours in Country.
+#                     The Astroverse team.""")
+
+
+# stra = 'aaboy' 
+# my_sentence = 'A quick brown, fox jumps over, a lazy dog'
+
+# #string method
+# print(stra.index('a'))
+# print(stra.rindex('a'))
+# print(stra.find('z'))
+# print(stra.rfind('o'))
+# print(stra.count('a'))
+# print(stra.replace('a', 'z'))
+# print(my_sentence.split())
+# print(my_sentence.split(','))
+
+
+# name = 'tundecv.pdf'
+# print(name.split('.')[-1]) 
+
+# num = ['0','4','3','2']
+# otp = ''.join(num)
+# print(otp)
+
+#s1='Ault'
+#s2='Kelly'
+#print('Original Strings are', s1, s2)
+
+#middle index number of s1
+#mi = len (s1) // 2
+
+#get character from 0 to the middle index number from s1
+#x= s1[:mi] + s2 + s1[mi:]
+
+#print(x)
+
+#strl= "Welcome to USA, usa awesome, isn't it?"
+
+#a = strl.lower()
+
+#print(a.count('usa'))
+
+#strl = 'Emma is a data scientist who knows Python. Emma works at google'
+
+#a = strl.rfind('Emma')
+#print(a)
+
+#strl= 'Emma-is-a-data-scientist'
+
+#print(strl.replace(('-'), (' ')))
+
+#split method #print(''.join(strl.split('-')))
+
+# strl = "/*Jon is @developer & musician"
+
+# a = strl.replace('/','').replace('*','').replace('@','').replace('& ','')
+
+# print(a)
+
+###LIST###
+#my_list=["I", "am", "good"]
+#a_list=["she", "is", "a", "queen"]
+
+#ab = my_list + a_list
+#ans =  ab[::2]
+#print(ans)
+
+
+#Given the list below:
+#list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+#Get 5000 and 500 out of the list and add item together.
+# Print the result on the terminal 
+# Keep slicing until there's nothing to slice anymore 
+
+list1 = [10, 
+    20, 
+        [300, 400, [5000, 6000], 500], 
+    30,
+    40 ]
+
+#a = list1[2]
+#b = a[2:] 
+
+#c = b[0]
+#d = c[0]
+
+#c1 = b[1]
+#e = d + c1
+#print(e)
+
+#print(list1[2][2][0] + list1[2][3])
+
+
+
+
+
+
+
